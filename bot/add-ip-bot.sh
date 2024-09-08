b7492dba3ecf10cd246ab7597629c6669c99be44
denisa32 <ellasusilo647@gmail.com>
2024-09-08 23:01:36 +0700

    Update add-ip-bot.sh

--- bot/add-ip-bot.sh
+++ bot/add-ip-bot.sh
@@ -17,7 +17,7 @@ checking_sc() {
     echo -e "   \033[0;33mYour VPS${NC} $IP \033[0;33mHas been Banned${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mWhatsapp${NC} wa.me/628981874211"
+    echo -e "      \033[0;36mWhatsapp${NC} wa.me/628388886316"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit
   fi
